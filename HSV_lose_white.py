bc2a74cfc3eda1f157f751f7922f3ce232761d21
Leon Erzberger <leon@erzberger.ch>
2019-10-16 17:42:38 +0200

    new version of correct number of tiles

--- HSV_lose_white.py
+++ HSV_lose_white.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import math as mt
 
 #prepare images
-cube1 = cv2.imread(r"C:\Users\le\SynologyDrive\Gymnasium\Maturarbeit\Wuerfel_normal_Test_2_1_1.jpg")
-cube2 = cv2.imread(r"C:\Users\le\SynologyDrive\Gymnasium\Maturarbeit\Wuerfel_normal_Test_2_1_2.jpg")
+cube1 = cv2.imread(r"C:\Users\le\SynologyDrive\Gymnasium\Maturarbeit\Wuerfel_normal_Test_1_1_1.jpg")
+cube2 = cv2.imread(r"C:\Users\le\SynologyDrive\Gymnasium\Maturarbeit\Wuerfel_normal_Test_1_1_2.jpg")
 scale_percent = 100 # percent of original size
 width = int(cube1.shape[1] * scale_percent / 100)
 height = int(cube1.shape[0] * scale_percent / 100)
@@ -21,7 +21,7 @@ resized_list = [resized1, resized2]
 #general
 coordinates = [[[[372,340],[329,245],[279,154],[261,244],[243,310],[268,391],[293,489],[329,432],[292,329]],[[468,279],[585,271],[674,261],[609,185],[548,117],[463,111],[359,105],[406,182],[516,182]],[[473,392],[418,467],[373,529],[469,506],[552,488],[609,431],[681,359],[583,377],[522,447]]],[[[402,278],[430,366],[451,435],[480,366],[504,301],[489,232],[469,147],[439,209],[464,291]],[[324,225],[372,158],[405,106],[320,115],[246,122],[197,169],[133,227],[222,226],[276,163]],[[310,307],[213,313],[127,312],[181,383],[227,439],[300,451],[377,465],[350,394],[258,391]]]]
 avgcol = [[[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]]
-colors = ["b", "w", "r", "g", "y", "o"]
+colors = ["w", "g", "r", "y", "b", "o"]
 tilecol_correct_1_1 = [[["r", "r", "o", "w", "w", "y", "o", "r", "w"], ["y", "y", "g", "y", "y", "w", "g", "w", "g"], ["b", "g", "g", "y", "r", "o", "w", "g", "r"]], [["r", "b", "b", "r", "r", "g", "b", "w", "y"], ["y", "g", "w", "o", "w", "b", "b", "b", "b"], ["g", "r", "o", "b", "y", "o", "o", "o", "o"]]]
 tilecol_correct_1_2 = [[["r", "b", "b", "r", "r", "g", "b", "w", "y"], ["g", "r", "o", "b", "y", "o", "o", "o", "o"], ["y", "g", "w", "o", "w", "b", "b", "b", "b"]], [["r", "r", "o", "w", "w", "y", "o", "r", "w"], ["b", "g", "g", "y", "r", "o", "w", "g", "r"], ["y", "y", "g", "y", "y", "w", "g", "w", "g"]]]
 tilecol_correct_2_1 = [[["y", "r", "o", "o", "b", "r", "o", "r", "b"], ["r", "w", "g", "o", "y", "o", "g", "b", "w"], ["g", "g", "w", "b", "w", "g", "o", "g", "r"]], [["w", "r", "r", "w", "w", "y", "g", "o", "g"], ["b", "g", "r", "w", "b", "w", "y", "b", "y"], ["r", "y", "o", "y", "y", "b", "b", "y", "o"]]]
@@ -128,30 +128,7 @@ for i in range(2):
                         tilenum[l] += 1
 print(tilecol)
 #correct number of each color
-for i in range(6):
-    while tilenum[i] > 9:
-        a1 = 0
-        a2 = 0
-        a3 = 0
-        counter = 0
-        big_dist = 0
-        breaker = 0
-        for j in range(2):
-            for k in range(3):
-                for l in range(9):
-                    if tilecol[j][k][l] == colors[i] and distances[j][k][l][i] > big_dist:
-                        big_dist = distances[j][k][l][i]
-                        a1 = j
-                        a2 = k
-                        a3 = l
-        while breaker == 0:
-            for j in range(6):
-                if tilenum[j] < 9 and distances[a1][a2][a3][j] < counter:
-                    tilecol[a1][a2][a3] = colors[j]
-                    tilenum[i] -= 1
-                    tilenum[j] += 1
-                    breaker = 1
-                else: counter += 1
+
 
 
 #method 2 for correct number
@@ -166,7 +143,7 @@ for i in range(6):
         breaker = 0
         for j in range(2):
             for k in range(3):
-                for l in range(9):
+                for l in range(8):
                     if tilecol[j][k][l] == colors[i]:
                         for m in range(6):
                             if tilenum[m] < 9 and distances[j][k][l][m] < small_dist:
@@ -186,7 +163,7 @@ error = 0
 for i in range(2):
     for j in range(3):
         for k in range(9):
-            if tilecol[i][j][k] != tilecol_correct_2_1[i][j][k]:
+            if tilecol[i][j][k] != tilecol_correct_1_1[i][j][k]:
                 error += 1
 
 
@@ -197,10 +174,10 @@ ax = fig.add_subplot(111, projection='3d')
 for i in range(2):
     for j in range(3):
         for k in range(9):
-            if tilecol_correct_2_1[i][j][k] == "r":
+            if tilecol_correct_1_1[i][j][k] == "r":
                 print(avgcol[i][j][k], "r")
                 ax.scatter(avgcol[i][j][k][0], avgcol[i][j][k][1], avgcol[i][j][k][2], color = 'red', marker = 'o')
-            elif tilecol_correct_2_1[i][j][k] == "o":
+            elif tilecol_correct_1_1[i][j][k] == "o":
                 ax.scatter(avgcol[i][j][k][0], avgcol[i][j][k][1], avgcol[i][j][k][2], color = 'orange', marker = 'o')
                 print(avgcol[i][j][k], "o")
 
@@ -216,7 +193,7 @@ for k in range(7):
 ax.grid(True)
 plt.show()
 print(tilecol)
-print(tilecol_correct_2_1)
+print(tilecol_correct_1_1)
 print(error)
 print(distances)
 
