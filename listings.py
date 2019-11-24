076f1a61ffbaeeb4575f10e5c2c29afabdf9f90e
Leon Erzberger <leon@erzberger.ch>
2019-11-24 20:05:16 +0100

    testing

--- listings.py
+++ listings.py
@@ -1,42 +1,42 @@
 # find average color of tiles
-for rgb in range(3):
+for rgb in range(3): # R-, G-, und B-Werte einzeln auslesen
     col = 0
     for m in range(20):
         for n in range(20):
-            col += picture[x - 10 + m][y - 10 + n][rgb]
-    avgcol[side][tile][rgb] = int(col / 400)
+            col += picture[x - 10 + m][y - 10 + n][rgb] # Werte einzelner Pixel addieren
+    avgcol[side][tile][rgb] = int(col / 400) # Durchschnitt in Liste speichern
 
 # evaluate color of each tile
 col = 200000
-for middle in range(6):
+for middle in range(6): # sechs Mitten durchgehen
     dist = 0
     for rgb in range(3):
         dist += (avgcol[side][tile][rgb] -
-                 avgcol[middle][8][rgb]) ** 2
+                 avgcol[middle][8][rgb]) ** 2 # Quadrate addieren
     distances[side][tile][middle] = mt.sqrt(dist)
-    if dist < col:
+    if dist < col: # testen, ob Mitte am naechsten ist
         col = dist
         tilecol[side][tile] = colors[middle]
 
 
 
-if avgcol[side][tile][1] < 70:
-    white_question += 1
-if avgcol[side][tile][2] < 0:
-    black_question += 1
+if avgcol[side][tile][1] < 50:
+    white_question += 1 # Variable, die weisse Felder zaehlt
+if avgcol[side][tile][2] < 50:
+    black_question += 1 # Variable, die schwarze Felder zaehlt
 
 
 
 if black_question == 9:
-    for side in range(6):
-        if avgcol[side][8][2] < lowvaluemid:
-            lowvaluemid = avgcol[side][8][2]
-            blackmidposition = side
-    tilecol[blackmidposition][8] = "black"
-    colors[blackmidposition] = "black"
-    tilenum[blackmidposition] = 9
-elif black_question != 0:
+    for middle in range(6):
+        if avgcol[middle][8][2] < lowvaluemid: # Vergleich, ob Value am tiefsten ist
+            lowvaluemid = avgcol[middle][8][2]
+            blackmidposition = middle
+elif black_question != 0: # Programmabbruchbedingung
     sys.exit("ERROR!")
+
+
+
 
 
 
@@ -50,17 +50,17 @@ for color in range(6):
         for side in range(6):
             for tile in range(9):
                 if tilecol[side][tile] == colors[color]:
-                    if big_dist < distances[side][tile][color]:
+                    if big_dist < distances[side][tile][color]: # Vergleich von aktuell groesster Distanz und Distanz von Seite
                         big_dist = distances[side][tile][color]
                         sidetpos = side
                         tilepos = tile
         for diffcol in range(6):
             if tilenum[diffcol] < 9:
-                if small_dist > distances[sidetpos][tilepos][diffcol]:
+                if small_dist > distances[sidetpos][tilepos][diffcol]: # Zuordnen zu naechster Mitte
                     small_dist = distances[sidetpos][tilepos][diffcol]
                     colpos = diffcol
-        tilecol[sidetpos][tilepos] = colors[colpos]
-        tilenum[colpos] += 1
+        tilecol[sidetpos][tilepos] = colors[colpos] # Farbe wird geaendert
+        tilenum[colpos] += 1 # Anzahl Felder wird angepasst
         tilenum[color] -= 1
 
 
@@ -79,11 +79,11 @@ for color in range(6):
                 if tilecol[side][tile] == colors[color]:
                     for diffcol in range(6):
                         if tilenum[diffcol] < 9 and distances[side][tile][
-                            diffcol] < small_dist:
+                            diffcol] < small_dist: # Abstand wird verglichen
                             small_dist = distances[side][tile][diffcol]
-                            sidepos = side
+                            sidepos = side # Feld wird gespeichert
                             tilepos = tile
-                            colpos = diffcol
-        tilecol[sidepos][tilepos] = colors[colpos]
-        tilenum[color] -= 1
+                            colpos = diffcol # Farbe wird gespeichert
+        tilecol[sidepos][tilepos] = colors[colpos] # Farbe wird geaendert
+        tilenum[color] -= 1 # Anzahl Felder wird angepasst
         tilenum[colpos] += 1
